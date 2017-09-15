66fb485d12fd382e50dfdc1f39f95bc550eeca30
walker-leee <1255968521@qq.com>
2017-09-15 17:13:53 +0800

    我更新了一些知识点...

--- first.js
+++ first.js
@@ -4,6 +4,20 @@ function sum (a, b) {
 };
 
 sum(1, 2);
+sum(2, 3);
+//  git init    初始化
+
+//  git config --global user.name
+//  git config --global user.email
 
 //  git diff    查看工作区和版本库之间的差异
 //  git status   查看状态
+
+// git add 文件名  (-A  --all) 全部添加的参数
+// git commit -m "提交的相关信息"
+
+//  假设，我先在感觉前一个提交的文件，有误, 想撤回
+//  首先查看历史记录
+//  git log 来看
+//  git reset --hard 提交id
+//  查看ref记录
